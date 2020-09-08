1d4963f45e6cedd8afab117bd49c9bf60ea567ee
Justin Clayton <jt.clayton92@yahoo.com>
2020-09-07 20:14:16 -0700

    Migrate previous work on array list

--- CSDataStructs.Code/ArrayList.cs
+++ CSDataStructs.Code/ArrayList.cs
@@ -1,8 +1,90 @@
 ﻿using System;
+using System.Text;
 
 namespace CSDataStructs.Code
 {
-    public class ArrayList
+    public class ArrayList<T>
     {
+        int _size;
+        int _maxSize;
+        int _start;
+        T[] _arr;
+
+        public int Size
+        {
+            get { return _size; }
+        }
+
+        public ArrayList()
+        {
+            Clear();
+        }
+
+        public void Clear()
+        {
+            _size = 0;
+            _start = 4;
+            _maxSize = 8;
+            _arr = new T[8];
+        }
+
+        public T Get(int index)
+        {
+            if (index < 0 || index >= _size)
+            {
+                throw new IndexOutOfRangeException($"Index {index} out of bounds");
+            }
+            return (T) _arr[_start + index];
+        }
+
+        public void InsertFront(T item)
+        {
+            checkOverCapacity();
+            _start--;
+            _arr[_start] = item;
+            _size++;
+        }
+
+        public void InsertBack(T item)
+        {
+            checkOverCapacity();
+            _arr[_start + _size] = item;
+            _size++;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("[");
+
+            for (int i = _start; i < (_start + _size - 1); i++) {
+                sb.Append($"{_arr[i]}, ");
+            }
+
+            if (_size > 0) {
+                sb.Append(_arr[(_start + _size - 1)]);
+            }
+            sb.Append("]");
+            return sb.ToString();
+        }
+
+        private void checkOverCapacity() {
+            if (_start < _maxSize / 4 || (_maxSize - _start - _size) <= (_maxSize / 4) || _size > _maxSize / 4 * 3) {
+                extend();
+            }
+        }
+
+        private void extend() {
+            int newMax = _maxSize * 4;
+            int newStart = (newMax - _size) / 2;
+            T[] newArr = new T[newMax];
+            for (int i = 0; i < _size; i++) {
+                newArr[newStart + i] = _arr[_start + i];
+            }
+            _arr = newArr;
+            _maxSize = newMax;
+            _start = newStart;
+        }
     }
 }
--- CSDataStructs.Tests/ArrayListTests.cs
+++ CSDataStructs.Tests/ArrayListTests.cs
@@ -1,14 +1,120 @@
 using System;
 using Xunit;
+using CSDataStructs.Code;
 
 namespace CSDataStructs.Tests
 {
     public class ArrayListTests
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly ArrayList<int> list;
+        private Random rng;
 
+        public ArrayListTests()
+        {
+            list = new ArrayList<int>();
+            rng = new Random();
+        }
+
+        [Fact]
+        public void TestConstructor()
+        {
+            Assert.Equal(0, list.Size);
+        }
+
+        [Fact]
+        public void TestToString()
+        {
+            Assert.Equal("[]", list.ToString());
+        }
+
+        [Fact]
+        public void TestClear()
+        {
+            list.InsertBack(4);
+            list.InsertBack(3);
+            list.Clear();
+            Assert.Equal(0, list.Size);
+            Assert.Equal("[]", list.ToString());
+        }
+
+        [Fact]
+        public void TestInsertFront()
+        {
+            list.InsertFront(1);
+            Assert.Equal(1, list.Size);
+            Assert.Equal("[1]", list.ToString());
+            list.InsertFront(2);
+            Assert.Equal(2, list.Size);
+            Assert.Equal("[2, 1]", list.ToString());
+        }
+
+        [Fact]
+        public void TestInsertFrontMany()
+        {
+            buildBigOrderedList("front");
+            Assert.Equal(100, list.Size);
+        }
+
+        [Fact]
+        public void TestInsertBack()
+        {
+            list.InsertBack(1);
+            Assert.Equal(1, list.Size);
+            Assert.Equal("[1]", list.ToString());
+            list.InsertBack(2);
+            Assert.Equal(2, list.Size);
+            Assert.Equal("[1, 2]", list.ToString());
+        }
+
+        [Fact]
+        public void TestInsertBackMany()
+        {
+            buildBigOrderedList("back");
+            Assert.Equal(100, list.Size);
+        }
+
+        [Fact]
+        public void TestGet()
+        {
+            list.InsertBack(1);
+            Assert.Equal(1, list.Get(0));
+            list.InsertFront(5);
+            Assert.Equal(1, list.Get(1));
+        }
+
+        [Fact]
+        public void TestGetOutOfRange()
+        {
+            list.InsertBack(1);
+            list.InsertBack(2);
+            Assert.Throws<IndexOutOfRangeException>(() => list.Get(-1));
+            Assert.Throws<IndexOutOfRangeException>(() => list.Get(2));
+        }
+
+        [Fact]
+        public void TestGetMany()
+        {
+            buildBigOrderedList("back");
+            for (int i = 0; i < 10; i++)
+            {
+                int index = rng.Next(100);
+                Assert.Equal(index, list.Get(index));
+            }
+        }
+
+        private void buildBigOrderedList(string side)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (side == "front")
+                {
+                    list.InsertFront(i);
+                }
+                else
+                {
+                    list.InsertBack(i);
+                }
+            }
         }
     }
 }
