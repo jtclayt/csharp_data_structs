59ac38742af8ed9acdfbf69a9bf1f68df37db26f
Justin Clayton <jt.clayton92@yahoo.com>
2020-09-09 21:09:17 -0700

    Complete LinkedList data structure

--- CSDataStructs.Code/LinkedList.cs
+++ CSDataStructs.Code/LinkedList.cs
@@ -7,6 +7,7 @@ namespace CSDataStructs.Code
     {
         int _size;
         Node _head;
+        Node _tail;
 
         public int Size
         {
@@ -34,51 +35,155 @@ namespace CSDataStructs.Code
         {
             _size = 0;
             _head = null;
+            _tail = null;
         }
 
         public T Get(int index)
         {
-            throw new NotImplementedException();
+            if (index < 0 || index >= _size)
+            {
+                throw new IndexOutOfRangeException($"Index {index} out of bounds");
+            }
+            return iterate(index).Value;
         }
 
         public void InsertFront(T item)
         {
-            throw new NotImplementedException();
+            _head = new Node(item, _head);
+            _size++;
+            if (_size == 1)
+            {
+                _tail = _head;
+            }
         }
 
         public void InsertBack(T item)
         {
-            throw new NotImplementedException();
+            if (_size == 0)
+            {
+                InsertFront(item);
+            }
+            else
+            {
+                _tail.Next = new Node(item);
+                _tail = _tail.Next;
+                _size++;
+            }
         }
 
         public void InsertAt(int index, T item)
         {
-            throw new NotImplementedException();
+            if (index < 0 || index > _size)
+            {
+                throw new IndexOutOfRangeException($"Index {index} out of bounds");
+            }
+            else if (index == 0)
+            {
+                InsertFront(item);
+            }
+            else if (index == _size)
+            {
+                InsertBack(item);
+            }
+            else
+            {
+                Node nodeBefore = iterate(index-1);
+                nodeBefore.Next = new Node(item, nodeBefore.Next);
+                _size++;
+            }
         }
 
         public T RemoveFront()
         {
-            throw new NotImplementedException();
+            if (_size == 0)
+            {
+                throw new IndexOutOfRangeException("List is empty");
+            }
+            T temp = _head.Value;
+            _head = _head.Next;
+            _size--;
+            if (_size == 0)
+            {
+                _tail = null;
+            }
+            return temp;
         }
 
         public T RemoveBack()
         {
-            throw new NotImplementedException();
+            if (_size == 0)
+            {
+                throw new IndexOutOfRangeException("List is empty");
+            }
+            else if (_size == 1)
+            {
+                return RemoveFront();
+            }
+            T temp = _tail.Value;
+            Node newTail = iterate(_size - 2);
+            _tail = newTail;
+            _size--;
+            return temp;
         }
 
         public T RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            if (index < 0 || index >= _size)
+            {
+                throw new IndexOutOfRangeException($"Index {index} out of bounds");
+            }
+            else if (index == 0)
+            {
+                return RemoveFront();
+            }
+            else if (index == _size - 1)
+            {
+                return RemoveBack();
+            }
+            Node prevNode = iterate(index - 1);
+            T temp = prevNode.Next.Value;
+            prevNode.Next = prevNode.Next.Next;
+            _size--;
+            return temp;
         }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            Node curr = _head;
+            while (curr != null)
+            {
+                if (curr.Value.Equals(item))
+                {
+                    return true;
+                }
+                curr = curr.Next;
+            }
+            return false;
         }
 
         public override string ToString()
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+            Node curr = _head;
+
+            while (curr != null)
+            {
+                sb.Append($"{curr.Value} -> ");
+                curr = curr.Next;
+            }
+
+            return sb.ToString();
+        }
+
+        private Node iterate(int index)
+        {
+            Node curr = _head;
+            while (index > 0)
+            {
+                curr = curr.Next;
+                index--;
+            }
+            return curr;
         }
     }
 }
